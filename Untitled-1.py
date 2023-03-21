cb135e4f9f5b1b8da474725d836a1b0ed12ae019
sqkllx <128507856+sqkllx@users.noreply.github.com>
2023-03-21 21:48:29 +0530

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,14 @@
+print ("Simple Calculator") #Use of print statement
+x=10 #Integer data type can be changed
+y=20 #Integer data type can be changed
+
+sum = x + y #Sum of x and y
+subtraction = x - y #Difference of x and y
+multiplication = x * y #Product of x and y
+division = x / y #Quotient of x and y
+remainder = x % y #remainder of x and y
+print ("The sum of x and y", sum)
+print ("The difference of x and y", subtraction)
+print ("The product of x and y", multiplication)
+print ("The quotient of x and y", division)
+print ("The remainder of x and y", remainder) 
